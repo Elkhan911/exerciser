1f4e58b7ea6fec8d46424584ee55ffe5cefcaaba
Elkhan <955pro@mail.ru>
2022-03-17 02:48:35 +0500

    1

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-let phrases = [];
+function searchForTheMeaningOfLife() {
+  const meaningOfLifeFound = true;
+}
+
+const meaningOfLife = searchForTheMeaningOfLife();
+
+/* Поскольку функция ничего не возвращает,
+в переменную meaningOfLife попадёт undefined. */
+
+console.log(meaningOfLife); // undefined
