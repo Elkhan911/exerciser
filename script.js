130cf75ee93057a5be1a2172fe242b5de711b187
Elkhan <955pro@mail.ru>
2022-04-16 01:38:51 +0500

    dom topic - second part

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
-let password = prompt("Введите пароль:");
+const container = document.querySelector(".container");
+const songsContainer = container.querySelector(".songs-container");
+const addButton = container.querySelector(".input__btn_action_add");
+const resetButton = container.querySelector(".input__btn_action_reset");
+const noSongsElement = container.querySelector(".no-songs");
 
-if (password === 0) {
-  password = prompt("Пароль обязателен и не может быть пустым.");
+function renderHasSongs() {
+  resetButton.removeAttribute("disabled");
+  resetButton.classList.remove("input__btn_disabled");
+  noSongsElement.classList.add("no-songs_hidden");
 }
+
+function renderNoSongs() {
+  resetButton.setAttribute("disabled", true);
+  resetButton.classList.add("input__btn_disabled");
+  noSongsElement.classList.remove("no-songs_hidden");
+}
+
+function addSong(artistValue, titleValue) {
+  const trackContainer = document.createElement("div");
+  trackContainer.classList.add("song");
+
+  const artistElement = document.createElement("h4");
+  artistElement.classList.add("song__artist");
+  artistElement.textContent = artistValue;
+
+  const titleElement = document.createElement("h4");
+  titleElement.classList.add("song__title");
+  titleElement.textContent = titleValue;
+
+  const likeButtonElement = document.createElement("button");
+  likeButtonElement.classList.add("song__like");
+}
+
+addButton.addEventListener("click", function () {
+  const artist = document.querySelector(".input__text_type_artist");
+  const title = document.querySelector(".input__text_type_title");
+
+  addSong(artist.value, title.value);
+  renderHasSongs();
+
+  artist.value = "";
+  title.value = "";
+});
--- uni script.js
+++ uni script.js
@@ -1,5 +1,55 @@
-let password = prompt("Введите пароль:");
+const container = document.querySelector(".container");
+const songsContainer = container.querySelector(".songs-container");
+const addButton = container.querySelector(".input__btn_action_add");
+const resetButton = container.querySelector(".input__btn_action_reset");
+const noSongsElement = container.querySelector(".no-songs");
 
-if (!password) {
-  password = prompt("Пароль обязателен и не может быть пустым.");
+function renderHasSongs() {
+  resetButton.removeAttribute("disabled");
+  resetButton.classList.remove("input__btn_disabled");
+  noSongsElement.classList.add("no-songs_hidden");
 }
+
+function renderNoSongs() {
+  resetButton.setAttribute("disabled", true);
+  resetButton.classList.add("input__btn_disabled");
+  noSongsElement.classList.remove("no-songs_hidden");
+}
+
+function addSong(artistValue, titleValue) {
+  const trackContainer = document.createElement("div");
+  trackContainer.classList.add("song");
+
+  const artistElement = document.createElement("h4");
+  artistElement.classList.add("song__artist");
+  artistElement.textContent = artistValue;
+
+  const titleElement = document.createElement("h4");
+  titleElement.classList.add("song__title");
+  titleElement.textContent = titleValue;
+
+  const likeButtonElement = document.createElement("button");
+  likeButtonElement.classList.add("song__like");
+
+  songsContainer.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class="song">
+      <h4 class="song__artist">${artistValue}</h4>
+      <p class="song__title">${titleValue}</p>
+      <button class="song__like"></button>
+    </div>
+  `
+  );
+}
+
+addButton.addEventListener("click", function () {
+  const artist = document.querySelector(".input__text_type_artist");
+  const title = document.querySelector(".input__text_type_title");
+
+  addSong(artist.value, title.value);
+  renderHasSongs();
+
+  artist.value = "";
+  title.value = "";
+});
