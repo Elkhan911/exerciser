e34af2ae369bb3e4106c462417e624d37da858f1
Elkhan911 <955pro@mail.ru>
2022-04-22 20:31:17 +0500

    yeah

--- uni script.js
+++ uni script.js
@@ -1,13 +1,5 @@
-let employee = {
-  firstName: "Василий",
-  lastName: "Тёркин",
-  age: 28,
+const building = {
+  floors: 4,
+  hasElevator: false,
+  type: "панельный",
 };
-
-console.log(employee);
-
-function getFullName() {
-  console.log(employee.firstName + " " + employee.lastName);
-}
-
-getFullName(employee);
