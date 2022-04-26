9405c43219ffe9db0f1c08cf71c037335d4e6781
Elkhan911 <955pro@mail.ru>
2022-04-26 11:00:30 +0300

    SPB

---
+++ script.js
@@ -0,0 +1,10 @@
+const russian = "Добро пожаловать";
+const english = "Welcome";
+const french = "Bienvenue";
+const italian = "Benvenuto";
+const spanish = "bienvenido";
+const chinese = "歡迎";
+
+const welcomeMessages = {
+  // напишите свой код здесь
+};
--- uni script.js
+++ uni script.js
@@ -1,5 +1,8 @@
-const building = {
-  floors: 4,
-  hasElevator: false,
-  type: "панельный",
-};
+const russian = 'Добро пожаловать';
+const english = 'Welcome';
+const french = 'Bienvenue';
+const italian = 'Benvenuto';
+const spanish = 'bienvenido';
+const chinese = '歡迎';
+
+const welcomeMessages = { russian, english, french, italian, spanish, chinese };
